4158f3cebf4186ceaa4949c3bf673c433c6390c7
Ankit Dhotre <ankitdhotre21@gmail.com>
2026-03-09 22:22:08 +0530

    Add : html tag for google searrch console

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="5e8WyB2v9U1_-Mu4zr4_z7TyC_GkKi2-I1DvdxiGitA" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
